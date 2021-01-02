8aea1ab790c607a0eadf4953fe2573b040e674ae
Khusnul Hisyam <khsyaam62@gmail.com>
2021-01-02 23:40:52 +0700

    V.1

---
+++ resources/views/users/master.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="zxx">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+    <meta name="author" content="Creative Tim">
+    <title>@yield('title')</title>
+    <!-- Favicon -->
+    <link rel="icon" href="{{asset('/assets')}}/img/botika.webp" type="image/png">
+    {{-- <link rel="icon" href="{{asset('/assets')}}/img/logo.svg"> --}}
+    @include('users/header')
+
+    <section class="banner-part">
+        @yield('content')
+        @include('users/footer')
+    </section>
+
+    
+</body>
+</html>
